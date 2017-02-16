a6f4be996643805081867b91d994672bd68fb4a2
kujenga <ataylor0123@gmail.com>
2017-02-16 16:17:45 -0500

    add basic auth capabilities

--- simpleproxy.go
+++ simpleproxy.go
@@ -11,6 +11,8 @@ import (
 var (
 	target = flag.String("target", "", "proxy target")
 	addr   = flag.String("addr", ":6600", "address to listed on")
+	user   = flag.String("username", "", "username for basic authentication")
+	pass   = flag.String("password", "", "password for basic authentication")
 )
 
 func main() {
@@ -28,5 +30,28 @@ func main() {
 	log.Printf("Proxying to %s on %s", tgt.String(), *addr)
 
 	proxy := httputil.NewSingleHostReverseProxy(tgt)
+	if *user != "" || *pass != "" {
+		proxy.Transport = &basicAuthTransport{
+			Username: *user,
+			Password: *pass,
+		}
+	}
+
 	log.Fatal(http.ListenAndServe(*addr, proxy))
+}
+
+// based on net/http/httputil internals
+
+type basicAuthTransport struct {
+	Username string
+	Password string
+}
+
+func (t basicAuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(t.Username, t.Password)
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+func (t *basicAuthTransport) Client() *http.Client {
+	return &http.Client{Transport: t}
 }
